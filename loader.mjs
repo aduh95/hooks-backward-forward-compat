731fe3e12e338548a689bfafc6b6214e99644943
Antoine du Hamel <duhamelantoine1995@gmail.com>
2023-09-06 18:06:40 +0200

    fix to make Node.js 18.x and 20.x behave the same

--- loader.mjs
+++ loader.mjs
@@ -1,17 +1,35 @@
 let port;
-console.log("loader loading");
 export function globalPreload(data) {
   port = data.port;
+  port.postMessage({ type: "globalPreload" });
   return `\
   globalThis.getPortFromLoader(port)
     `;
 }
 
 export function initialize(data) {
-  port ??= data?.port;
+  if (data == null) {
+    // Legacy --loader flag was used, ignore
+    return;
+  }
+  port = data.port;
+  port.postMessage({ type: "initialize" });
 }
 
 export function resolve(referrer, context, next) {
-  port?.postMessage({ referrer });
+  if (port == null) {
+    // Legacy --loader flag was used, ignore
+    return next(referrer, context);
+  }
+  port.postMessage({ type: "resolve", referrer, url: import.meta.url });
   return next(referrer, context);
+}
+
+export function load(url, context, next) {
+  if (port == null) {
+    // Legacy --loader flag was used, ignore
+    return next(url, context);
+  }
+  port.postMessage({ type: "load", url, url: import.meta.url });
+  return next(url, context);
 }
--- register.cjs
+++ register.cjs
@@ -1,7 +1,9 @@
 "use strict";
 const Module = require("node:module");
 const { pathToFileURL } = require("node:url");
-const { MessageChannel } = require("node:worker_threads");
+const { MessageChannel, isMainThread } = require("node:worker_threads");
+
+if (!isMainThread) return;
 
 const messageHandler = (msg) => {
   console.log("received from loader", msg);
@@ -18,14 +20,21 @@ if (Module.register) {
 
   port1.on("message", messageHandler);
 
-  Module.register("./loader.mjs", `${pathToFileURL(__filename)}`, {
-    data: { port: port2 },
-    transferList: [port2],
-  });
+  Module.register(
+    "./loader.mjs?register=true",
+    `${pathToFileURL(__filename)}`,
+    {
+      data: { port: port2 },
+      transferList: [port2],
+    }
+  );
+
+  port1.unref();
 } else {
   // On Node.js <20.6, we need to get the port from the globalPreload hook.
   globalThis.getPortFromLoader = (port) => {
     port.on("message", messageHandler);
+    port.unref();
     delete globalThis.getPortFromLoader;
   };
 }
